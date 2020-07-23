e8fe51da2d62a00aa5b4f3a6f72d8e7fc4ac4039
Alexandre Bulté <alexandre@bulte.net>
2020-07-23 13:02:36 +0200

    fix deploy url

--- .github/actions/dokku-preview-create-action/entrypoint.sh
+++ .github/actions/dokku-preview-create-action/entrypoint.sh
@@ -35,5 +35,5 @@ GIT_COMMAND="git push --force dokku@$HOST:$APP_NAME HEAD:refs/heads/master"
 echo "GIT_SSH_COMMAND="$GIT_SSH_COMMAND" $GIT_COMMAND"
 GIT_SSH_COMMAND="$GIT_SSH_COMMAND" $GIT_COMMAND
 
-URL=$APP_NAME.app.etalab.studio
+URL="http://$APP_NAME.app.etalab.studio"
 echo "::set-output name=url::$URL"
